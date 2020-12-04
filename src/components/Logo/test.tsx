198ec0cea32645618ad11a267bc85a46c0e4596a
zagodi <diego.s.zago1995@gmail.com>
2020-12-03 21:12:25 -0300

    Add FormSignIn

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,16 +1,16 @@
 import { screen } from '@testing-library/react'
-
 import { renderWithTheme } from 'utils/tests/helpers'
-import 'jest-styled-components'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render a white label by default', () => {
-    // renderizar o componente 'render
-    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel...
-    // expect - assertion
+  it('should render the logo with id passed', () => {
+    const { container } = renderWithTheme(<Logo id="myId" />)
 
+    expect(container.querySelector('#paint_linear_myId')).toBeInTheDocument()
+  })
+
+  it('should render a white label by default', () => {
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#FAFAFA'
@@ -24,17 +24,17 @@ describe('<Logo />', () => {
     })
   })
 
-  it('should render a bigger logo', () => {
-    renderWithTheme(<Logo size="large" />)
-    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
-      width: '20rem'
-    })
-  })
-
   it('should render a normal logo when size is default', () => {
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
     })
   })
 
