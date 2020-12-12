a347929a73b6d893600c9ac8664f9dd8cf505fdc
zagodi <diego.s.zago1995@gmail.com>
2020-12-12 20:39:24 -0300

    Add TextContent component

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -3,7 +3,7 @@ import media from 'styled-media-query'
 
 import { HeadingProps, LineColors } from '.'
 
-const wrapperModifiers = {
+export const wrapperModifiers = {
   lineleft: (theme: DefaultTheme, lineColor: LineColors) => `
     padding-left: ${theme.spacings.xxsmall};
     border-left: 0.7rem solid ${theme.colors[lineColor]};
