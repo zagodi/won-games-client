631789f03bb86ac984216d17d3e091a21bad8e0a
zagodi <diego.s.zago1995@gmail.com>
2022-07-14 20:12:23 -0300

    chore: create PaymentOptions component

--- src/components/PaymentOptions/test.tsx
+++ src/components/PaymentOptions/test.tsx
@@ -1,13 +1,50 @@
-import { render, screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import PaymentOptions from '.'
+import cards from './mock'
 
 describe('<PaymentOptions />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<PaymentOptions />)
+  it('should render the saved card options and the add new card button', () => {
+    renderWithTheme(<PaymentOptions cards={cards} handlePayment={jest.fn} />)
 
-    expect(screen.getByRole('heading', { name: /PaymentOptions/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/4325/)).toBeInTheDocument()
+    expect(screen.getByLabelText(/4326/)).toBeInTheDocument()
+    expect(screen.getByText(/add a new credit card/i)).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should handle select when clicking on the label', async () => {
+    renderWithTheme(<PaymentOptions cards={cards} handlePayment={jest.fn} />)
+
+    userEvent.click(screen.getByLabelText(/4325/))
+    await waitFor(() => {
+      expect(screen.getByRole('radio', { name: /4325/ })).toBeChecked()
+    })
+  })
+
+  it('should not call handlePayment when button is disabled', async () => {
+    const handlePayment = jest.fn()
+    renderWithTheme(
+      <PaymentOptions cards={cards} handlePayment={handlePayment} />
+    )
+
+    userEvent.click(screen.getByRole('button', { name: /buy now/i }))
+    expect(handlePayment).not.toHaveBeenCalled()
+  })
+
+  it('should call handlePayment when card is selected', async () => {
+    const handlePayment = jest.fn()
+    renderWithTheme(
+      <PaymentOptions cards={cards} handlePayment={handlePayment} />
+    )
+
+    userEvent.click(screen.getByLabelText(/4325/))
+
+    userEvent.click(screen.getByRole('button', { name: /buy now/i }))
+
+    await waitFor(() => {
+      expect(handlePayment).toHaveBeenCalled()
+    })
   })
 })
