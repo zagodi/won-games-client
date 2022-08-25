72c0fbe2d37ca4d776450f0a8214816d3bcf4f98
diegozagobloom <diego.zago@bloom.group>
2022-08-25 20:53:37 -0300

    chore: create queries and get Game info

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -4,6 +4,7 @@ import filterItemsMock from 'components/ExploreSidebar/mock'
 import { initializeApollo } from 'utils/apollo'
 
 import { QUERY_GAMES } from 'graphql/queries/games'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
@@ -12,7 +13,7 @@ export default function GamesPage(props: GamesTemplateProps) {
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query({
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
     variables: {
       limit: 9
@@ -24,6 +25,7 @@ export async function getStaticProps() {
       revalidate: 60,
       games: data.games.map((game) => ({
         title: game.name,
+        slug: game.slug,
         developer: game.developers[0].name,
         img:
           // `http://localhost:1337${game.cover?.url}` ||
